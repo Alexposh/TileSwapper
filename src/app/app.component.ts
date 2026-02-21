eaa63187fc500f6eed1d17c2bc854bb1b7ff448a
Posirca Alexandru <76157684+Alexposh@users.noreply.github.com>
2026-02-21 23:49:18 +0000

    ng update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import "konva/canvas-backend";
+import Konva from 'konva';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,35 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TileSwapper';
+  stage: Konva.Stage | undefined;
+  layer: Konva.Layer | undefined;
+
+
+
+  ngOnInit(): void {
+    
+    this.stage = new Konva.Stage({
+      width: 880,
+      height: 880,
+    });
+    this.layer = new Konva.Layer();
+
+    const firstSquare = new Konva.Rect({
+      height: 150,
+      width: 150,
+      fill: 'gray',
+      stroke: 'yellow',
+      strokeWidth: 3,
+    });
+
+    this.layer.add(firstSquare);
+
+    this.stage.add(this.layer);
+
+    // this.layer.draw();
+  }
+
+  
 }
